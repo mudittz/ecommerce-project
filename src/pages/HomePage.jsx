59f901833540b4edc71c10e541d7b2905743a7be
Mudit <muditmm90@gmail.com>
2026-08-06 20:38:31 +0530

    Fetch data fromthe backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,16 @@ import CheckmarkIcon from '../assets/images/checkmark.png'
 import './HomePage.css';
 export function HomePage() {
 
+    fetch('http://localhost:3000/api/products')
+        .then((response) => {
+            return response.json();
+        }).then((data) => {
+            console.log(data)
+
+        });
+
+
+
     return (
         <>
             <title>Homepage</title>
@@ -17,9 +27,9 @@ export function HomePage() {
                 <div className="products-grid">
                     {products.map((product) => {
                         return (
-                            <div 
-                            key= {product.id}
-                            className="product-container">
+                            <div
+                                key={product.id}
+                                className="product-container">
                                 <div className="product-image-container">
                                     <img className="product-image"
                                         src={product.image} />
@@ -31,14 +41,14 @@ export function HomePage() {
 
                                 <div className="product-rating-container">
                                     <img className="product-rating-stars"
-                                        src={`images/ratings/rating-${product.rating.stars * 10}.png` }/>
+                                        src={`images/ratings/rating-${product.rating.stars * 10}.png`} />
                                     <div className="product-rating-count link-primary">
                                         {product.rating.count}
                                     </div>
                                 </div>
 
                                 <div className="product-price">
-                                   ${(product.priceCents / 100).toFixed(2)}
+                                    ${(product.priceCents / 100).toFixed(2)}
                                 </div>
 
                                 <div className="product-quantity-container">
@@ -72,7 +82,7 @@ export function HomePage() {
 
 
                     })}
-                    
+
                 </div>
             </div>
         </>
