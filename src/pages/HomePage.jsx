9c36b251117d46315e63c5021a0eec70f2f49532
Mudit <muditmm90@gmail.com>
2026-08-06 21:57:42 +0530

    Get cart from the backent and display quantity

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,14 +6,18 @@ import CheckmarkIcon from '../assets/images/checkmark.png'
 
 import './HomePage.css';
 export function HomePage() {
-    const[products,setProducts] = useState(
-        []
-    );
+    const[products,setProducts] = useState([]);
+    const [cart,setCart] = useState([])
+
     useEffect(()=>{
          axios.get('http://localhost:3000/api/products')
             .then((response) => {
                 setProducts(response.data);
             });
+         axios.get('http://localhost:3000/api/cart-items')
+         .then((response)=>{
+            setCart(response.data);
+         })
         },[]);
 
     return (
@@ -22,7 +26,7 @@ export function HomePage() {
 
             <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
 
-            <Header />
+            <Header cart={cart}/>
 
             <div className="home-page">
                 <div className="products-grid">
